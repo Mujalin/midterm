ab163c1c92240700baf0d72e99bc17545c337488
Mujalin <59908391+Mujalin@users.noreply.github.com>
2020-03-24 10:57:20 +0700

    Update index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<HTML>
+<HEAD><TITLE> My Homepage </TITLE></HEAD>
+<BODY BGCOLOR=#FFFFFF>
+<H1>
+<?php echo "รหัสนิสิต-62310021"; ?><br>
+<?php echo "ชื่อ-นามสกุล มุจลินท์ ขัติยนนท์"; ?><br>
+<iframe width="500" height="500"
+src="https://youtu.be/xuTbtCz3Ysg">
+</iframe>
+</BODY>
+</HTML>

c3986762e200d79c4accc5452c3634c7ec080f45
Mujalin <59908391+Mujalin@users.noreply.github.com>
2020-03-24 11:07:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,6 @@
 <H1>
 <?php echo "รหัสนิสิต-62310021"; ?><br>
 <?php echo "ชื่อ-นามสกุล มุจลินท์ ขัติยนนท์"; ?><br>
-<iframe width="500" height="500"
-src="https://youtu.be/xuTbtCz3Ysg">
-</iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/xuTbtCz3Ysg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </BODY>
 </HTML>
